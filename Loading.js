2ed80139dd06ab9f60d26b3ce8b726f64c8afa62
Candice Han <jeeyoung.han82@gmail.com>
2020-08-11 22:52:28 +0200

    Add location feature

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#1a535c",
+  },
+  text: {
+    color: "whitesmoke",
+    fontSize: 30,
+  },
+});
